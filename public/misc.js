9f5ee4f4b1f56d3b773dbf6298299abb7ac36b40
William Johnson <joh15132@byui.edu>
2018-07-14 23:30:00 -0600

    append cannot be read....

--- public/misc.js
+++ public/misc.js
@@ -23,10 +23,23 @@ function handleResults(result){
     console.log("back from AJAX with result: " + result);
     if(data.length > 0) {
         var resultList = $("#data");
-        //resultList.empty();
+        resultList.empty();
     }
     
-    var raw = data;
-    resultList.append("<p>" + raw + "</p>");
+    var insert = "<table><th>Name</th><th>ESRB Rating</th><th>User Rating</th><th>Link to Reviews</th><th>Link to Help Center</th>";
+    for(var i = 0; i < data.length; i++){
+        insert += "<tr>" + data[i].name + "</tr>";
+        insert += "<tr>" + data[i].esrbrating + "</tr>";
+        insert += "<tr>" + data[i].userrating + "</tr>";
+        insert += "<tr> <button type='button' onclick='dosomething()'> Reviews </button></tr>";
+        insert += "<tr> <button type='button' onclick='dosomething()'> Help Center </button></tr>";
+    }
+    insert += "</table>";
+    console.log("inserting this table: " + insert);
+    resultList.append(insert);
     console.log("data appended to html page");
+}
+
+function dosomething(){
+    console.log("help or review center");
 }
